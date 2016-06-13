11068866ecb027ffb310770e059bb1f2bd2776c4
daiegon <diegorangels@gmail.com>
2016-06-13 03:06:00 -0300

    Versao 1 Raddar

---
+++ _build/sources/js/app.js
@@ -0,0 +1,11 @@
+$('.countdown').downCount({
+    date: '06/13/2016 23:59:59',
+    //offset: +10
+}, function () {
+    //alert('WOOT WOOT, done!');
+});
+
+$(document).ready(function () {
+
+
+});
